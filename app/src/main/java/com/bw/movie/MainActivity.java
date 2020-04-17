d9fd1e202abc772303e6aa00e3f9b9bd6470aca9
Mmeng Xuan <tr@xua.com>
2020-04-17 13:58:40 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bw.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author mengxuan
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
